acf6e084506cce31289f9f6b992839da033a4042
Montasar Dridi <montasardridi34@gmail.com>
2025-08-21 20:29:58 +0300

    refactor(io): made iter_images the only external function that should be called

--- blurx/utils/io.py
+++ blurx/utils/io.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import cv2
 
 
+# Internal Function | Used by iter_images function.
 def list_image_paths(path):
     """Return a list of image file paths from a single file or directory"""
     p = Path(path)
@@ -30,6 +31,7 @@ def list_image_paths(path):
     return image_path_list
 
 
+# Internal Function | Used by iter_images function.
 def load_image(path):
     """Load a single image and return a Numpy array"""
     image_array = cv2.imread(path, cv2.IMREAD_COLOR)
@@ -40,7 +42,8 @@ def load_image(path):
     return image_array
 
 
-def iter_images(path_list):
+def iter_images(path):
     """Read images one at a time. Keepa the memory usage low"""
-    for path in path_list:
-        yield path, load_image(path)
+    images_list = list_image_paths(path)
+    for image_path in images_list:
+        yield image_path, load_image(image_path)
